0cac5c4226c1e714d503efe64f5cf21e679d06d0
HF-Royo <74603650+HF-Royo@users.noreply.github.com>
2022-10-22 16:48:10 +0800

    Create site.php

---
+++ site.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+     <!-- Site.php is the name of the file -->
+     <form action = "site.php" method = "get">
+
+       Name: <br>
+       <input type="text" name="name">
+       <br>
+       <br>
+
+       Age:
+       <br>
+        <input type="number" name="age">
+        <input type="submit">
+
+     </form>
+
+     <br>
+     Your name is <?php echo $_GET["name"]?>
+     <br>
+     Your age is <?php echo $_GET["age"]?>
+
+  </body>
+</html>
